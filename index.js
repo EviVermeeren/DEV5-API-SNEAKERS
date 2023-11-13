a7a802b91af4041cd5ed14aed27c732252b8b43b
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-13 16:42:55 +0100

    feat: add login and update password logic

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
+const jwt = require("jsonwebtoken");
+const secretKey = "your_secret_key"; // Replace with your actual secret key
+
+const bcrypt = require("bcrypt");
+
 const mongoose = require("mongoose");
 mongoose.connect(
   "mongodb+srv://evivermeeren:wachtwoord@cluster0.plf34zk.mongodb.net/",
@@ -238,6 +243,85 @@ app.post("/api/v1/users", async (req, res) => {
     res.status(500).json({
       status: "error",
       message: "Failed to save user",
+    });
+  }
+});
+
+// Endpoint to log in
+app.post("/api/v1/login", async (req, res) => {
+  const { userName, userPassword } = req.body;
+
+  try {
+    // Check if the user exists in the database
+    const user = await User.findOne({ userName, userPassword });
+
+    if (!user) {
+      return res.status(401).json({
+        status: "error",
+        message: "Invalid credentials",
+      });
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({ userId: user._id }, secretKey, {
+      expiresIn: "1h",
+    });
+
+    // Send the token in the response
+    res.json({
+      status: "success",
+      message: "Login successful",
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+});
+
+app.put("/api/v1/users/:id/update-password", async (req, res) => {
+  const userId = req.params.id;
+  const { currentPassword, newPassword } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      });
+    }
+
+    // Check if the current password matches the one in the database
+    if (user.userPassword !== currentPassword) {
+      return res.status(401).json({
+        status: "error",
+        message: "Unauthorized: Incorrect current password",
+      });
+    }
+
+    // Update the user's password with the new one
+    user.userPassword = newPassword;
+
+    // Save the updated user to the database
+    await user.save();
+
+    res.json({
+      status: "success",
+      message: "Password updated successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
     });
   }
 });
