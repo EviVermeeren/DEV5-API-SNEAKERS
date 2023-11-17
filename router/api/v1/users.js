f63f922a775d197928922f808baffa0da12646dc
ArneSamson <arnesamson007@gmail.com>
2023-11-17 12:38:57 +0100

    feat: put index.js code in user router

--- router/api/v1/users.js
+++ router/api/v1/users.js
@@ -4,6 +4,14 @@ const router = express.Router();
 
 const userController = require("../../../controllers/api/v1/users.js");
 
+router.get("/", userController.index);
+
 router.post("/", userController.create);
+
+router.post("/login", userController.login);
+
+router.put("/:id/update-password", userController.updatePassword);
+
+router.post("/logout", userController.logout);
 
 module.exports = router;
