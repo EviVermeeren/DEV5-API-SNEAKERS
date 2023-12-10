1455add423579289ad6706fa5b21d5d4fc8bd437
Evi Vermeêren <evivermeeren@hotmail.com>
2023-12-10 15:56:19 +0100

    feat: include bcrypt

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 require("dotenv").config();
 const express = require("express");
-// const http = require("http");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const app = express();
 const port = 3000;
-// const WSPort = 3001;
 
 app.use(express.json());
 app.use(cors());
 
-// const jwt = require("jsonwebtoken");
-// const secretKey = "your_secret_key"; // Replace with your actual secret key
-
-mongoose.connect(
-  process.env.MONGODB,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "shoes",
-  }
-);
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "shoes",
+});
 
 const shoesRouter = require("./router/api/v1/shoes.js");
 const usersRouter = require("./router/api/v1/users.js");
@@ -30,12 +22,6 @@ const usersRouter = require("./router/api/v1/users.js");
 app.use("/api/v1/shoes", shoesRouter);
 app.use("/api/v1/users", usersRouter);
 
-// const server = http.createServer(app);
-// require('./primus/live.js').go(server);
-// server.listen(WSPort);
-
-
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-}); 
+});
