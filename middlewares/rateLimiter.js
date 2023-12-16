bf56182a60e5124b8140b457b01ed626971ba52f
Evi Vermeêren <evivermeeren@hotmail.com>
2023-12-16 17:11:02 +0100

    fix: rate limit up for testing fase

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,13 +2,13 @@ const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
+  max: 500, // dit moet naar beneden, nu voor testing fase hoog
   message: "Too many login attempts from this IP, please try again later.",
 });
 
 const createShoeLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 2,
+  max: 200, // dit moet naar beneden, nu voor testing fase hoog
   message:
     "Too many new shoe creation attempts from this IP, please try again later.",
 });
