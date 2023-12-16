fc5cf4ed78fca6abdd068d5e404ac910450a2187
Evi Vermeêren <evivermeeren@hotmail.com>
2023-12-16 19:07:52 +0100

    feat: change time to wait until login again

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,13 +1,13 @@
 const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 15 * 60 * 100,
   max: 5, // dit moet naar beneden, nu voor testing fase hoog
   message: "Too many login attempts from this IP, please try again later.",
 });
 
 const createShoeLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 15 * 60 * 100,
   max: 2, // dit moet naar beneden, nu voor testing fase hoog
   message:
     "Too many new shoe creation attempts from this IP, please try again later.",
