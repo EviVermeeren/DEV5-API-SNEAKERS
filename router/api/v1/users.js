05e85c7a602d31b2bdf64bbce0450963ca7aa668
Evi Vermeêren <evivermeeren@hotmail.com>
2023-12-16 17:00:51 +0100

    feat: add ratelimiter to how many login attemps you can make

--- router/api/v1/users.js
+++ router/api/v1/users.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const userController = require("../../../controllers/api/v1/users.js");
 const authenticate = require("../../../middlewares/authenticate");
 
+const { loginLimiter } = require("../../../middlewares/rateLimiter");
+
 router.get("/", authenticate, userController.index);
 
 router.post("/", userController.create);
 
-router.post("/login", userController.login);
+router.post("/login", loginLimiter, userController.login);
 
 router.put("/:id", authenticate, userController.updatePassword);
 
