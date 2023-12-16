5a9c3fa492c8fdc02f02ca32575908741072f857
Evi Vermeêren <evivermeeren@hotmail.com>
2023-12-16 13:05:53 +0100

    feat: add isAdmin true to model

--- models/User.js
+++ models/User.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    userName: String,
-    userPassword: String,
-    userEmail: String,
+  userName: String,
+  userPassword: String,
+  userEmail: String,
+  isAdmin: { type: Boolean, default: true },
 });
 
 const User = mongoose.model("User", userSchema);
